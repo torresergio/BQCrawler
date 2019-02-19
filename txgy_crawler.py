3c864da36bec59078feba3f2777c0ad5ca9761a7
jianwei <suijianwei@momenta.ai>
2019-02-19 21:37:24 +0800

    refact txgy

--- txgy_crawler.py
+++ txgy_crawler.py
@@ -6,14 +6,19 @@ import json
 import sys
 import datetime
 import time
-from imp import reload
+
+def deal_json(json_data, log_file):
+    for proj in json_data['plist']:
+        log_file.write('start time ' + proj['startTime'] + '\n')
+        log_file.write('end time ' + proj['endTime'] + '\n')
+        log_file.flush()
 
 # Request header.
 head = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
-    'Host': 'gongyi.qq.com',
+    'Host': 'ssl.gongyi.qq.com',
     'Accept-Language': 'en-GB,en;q=0.5',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept': '*/*',
 }
 
 # status : 1--募款中, 2--执行中, 3--已结束
@@ -22,12 +27,23 @@ status = (1, 2, 3)
 # tid : 71--疾病救助, 72--救灾扶贫, 73--教育助学, 74--自然保护, 75--其他
 tid = (71, 72, 73, 74, 75)
 
+log_file = open('/tmp/suijianwei', mode = 'a')
 for s_status in status:
     for s_tid in tid:
+        log_file.write(str(s_status) + " " + str(s_tid))
         for s_page in range(1, 100):
-            url_book = 'https://gongyi.qq.com/succor/project_list.htm#s_status=' + str(s_status) + '&s_tid=' + str(s_tid) + '&p=' + str(s_page)
-            print(url_book)
-            
-text = '\u7D27\u6025\u6551\u52A9\u4E2D\u5FC3\u4F9D\u6258\u57282011\u5E743\u6708\u5DF2\u5EFA\u7ACB\u7684\u751F\u547D'
-text.encode('utf-8')
-print(text)
+            url = 'https://ssl.gongyi.qq.com/cgi-bin/WXSearchCGI?ptype=stat&s_status='\
+                + str(s_status) +'&s_tid='+str(s_tid) + '&p=' + str(s_page)
+            res = requests.get(url, headers = head).text
+            deal_json(json.loads(res[1:-1]), log_file)         
+            '''
+            try:
+                url = 'https://ssl.gongyi.qq.com/cgi-bin/WXSearchCGI?ptype=stat&s_status='\
+                    + str(s_status) +'&s_tid='+str(s_tid) + '&p=' + str(s_page)
+                res = requests.get(url, headers = head).text
+                deal_json(json.loads(res[1:-1]), log_file)
+            except:
+                log_file.write('fxxk')
+                continue
+            '''
+log_file.close()
